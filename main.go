97acea94de0acdd0ee3779845f42452e9336cf1a
acytel <acytel0099@gmail.com>
2025-09-14 03:41:01 +0530

    Update

--- main.go
+++ main.go
@@ -10,13 +10,20 @@ import (
 func main() {
 	app := pocketbase.New()
 
-	app.OnAfterBootstrap().PreAdd(func(e *core.BootstrapEvent) error {
+	// This is a "lifecycle hook" that runs right before the web server starts.
+	// We are using it to print a confirmation message in the logs.
+	// The previous function `OnAfterBootstrap` was renamed to `OnBeforeServe`
+	// in a recent PocketBase update.
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		log.Println("----------------------------------------")
 		log.Println("  Project VISION AI Backend is starting...")
+		// e.Server.URL gives us the actual address the server is running on.
+		log.Println("  Admin UI will be available at:", e.Server.URL)
 		log.Println("----------------------------------------")
 		return nil
 	})
 
+	// Start the PocketBase framework.
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
 	}
