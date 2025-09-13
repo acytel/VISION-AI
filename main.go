1ad6d66a6d934fd65f1c6e57eec5e7d9011f6209
acytel <acytel0099@gmail.com>
2025-09-14 03:33:11 +0530

    Updated main.go

--- main.go
+++ main.go
@@ -1,19 +1,24 @@
 package main
+
 import (
-    "log"
-    "os"
-    "[github.com/pocketbase/pocketbase](https://github.com/pocketbase/pocketbase)"
-    "[github.com/pocketbase/pocketbase/core](https://github.com/pocketbase/pocketbase/core)"
+	"log"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
 )
+
 func main() {
-    app := pocketbase.New()
-    app.OnAfterBootstrap().PreAdd(func(e *core.BootstrapEvent) error {
-        log.Println("----------------------------------------")
-        log.Println("  Project VISION AI Backend is starting...")
-        log.Println("----------------------------------------")
-        return nil
-    })
-    if err := app.Start(); err != nil {
-        log.Fatal(err)
-    }
+	app := pocketbase.New()
+
+	app.OnAfterBootstrap().PreAdd(func(e *core.BootstrapEvent) error {
+		log.Println("----------------------------------------")
+		log.Println("  Project VISION AI Backend is starting...")
+		log.Println("----------------------------------------")
+		return nil
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
+
